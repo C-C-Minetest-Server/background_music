e0c441b8a60e0db53320011169258bf650725877
1F616EMO <root@1f616emo.xyz>
2024-07-03 09:12:12 +0800

    Fix no music

--- src/player.lua
+++ src/player.lua
@@ -102,7 +102,7 @@ function background_music.decide_and_play(player, instant)
         return
     end
 
-    if music ~= "keep" and (data[name] and data[name].music or "null") ~= music then
+    if not start_play_gap[name] and music ~= "keep" and (data[name] and data[name].music or "null") ~= music then
         -- Changing music, give it a gap
         background_music.set_start_play_gap(name, 2)
     end
