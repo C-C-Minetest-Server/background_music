74703558f2fd9bdd1268cb4f9d71722d674c9dd6
1F616EMO <root@1f616emo.xyz>
2024-07-03 08:58:21 +0800

    Fix nil value

--- src/player.lua
+++ src/player.lua
@@ -102,7 +102,7 @@ function background_music.decide_and_play(player, instant)
         return
     end
 
-    if data[name].music ~= music then
+    if data[name] and data[name].music ~= music then
         -- Changing music, give it a gap
         background_music.set_start_play_gap(name, 2)
     end
